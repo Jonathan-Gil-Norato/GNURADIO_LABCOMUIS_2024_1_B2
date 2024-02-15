4f347c0560e5b7a8aea862fb1c692f9274d5b521
Comu2_B2 <jonasdavig2016@gmail.com>
2024-02-15 09:14:49 -0500

    Primer avance lab1

--- practica_1/lab1/GNU_RADIOP1/gnuradiop1.py
+++ practica_1/lab1/GNU_RADIOP1/gnuradiop1.py
@@ -5,12 +5,10 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: Pulsos
-# Author: Estudiantes_UIS
-# Copyright: UIS
-# GNU Radio version: 3.10.1.1
+# Title: My_function
+# GNU Radio version: 3.9.8.0
 
-from packaging.version import Version as StrictVersion
+from distutils.version import StrictVersion
 
 if __name__ == '__main__':
     import ctypes
@@ -22,35 +20,31 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
-import os
-import sys
-sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
-
-from ModulacionPAM import ModulacionPAM  # grc-generated hier_block
 from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
 from gnuradio import analog
+from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.fft import window
+import sys
 import signal
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
-from gnuradio.qtgui import Range, RangeWidget
-from PyQt5 import QtCore
+import gnuradiop1_epy_block_0 as epy_block_0  # embedded python block
 
 
 
 from gnuradio import qtgui
 
-class Prueba_PAM(gr.top_block, Qt.QWidget):
+class gnuradiop1(gr.top_block, Qt.QWidget):
 
     def __init__(self):
-        gr.top_block.__init__(self, "Pulsos", catch_exceptions=True)
+        gr.top_block.__init__(self, "My_function", catch_exceptions=True)
         Qt.QWidget.__init__(self)
-        self.setWindowTitle("Pulsos")
+        self.setWindowTitle("My_function")
         qtgui.util.check_set_qss()
         try:
             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
@@ -68,7 +62,7 @@ class Prueba_PAM(gr.top_block, Qt.QWidget):
         self.top_grid_layout = Qt.QGridLayout()
         self.top_layout.addLayout(self.top_grid_layout)
 
-        self.settings = Qt.QSettings("GNU Radio", "Prueba_PAM")
+        self.settings = Qt.QSettings("GNU Radio", "gnuradiop1")
 
         try:
             if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
@@ -81,32 +75,16 @@ class Prueba_PAM(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 100e3
-        self.Fs = Fs = 1e3
-        self.Fm = Fm = 100
-        self.D = D = 10
-        self.Am = Am = 1
+        self.samp_rate = samp_rate = 32000
 
         ##################################################
         # Blocks
         ##################################################
-        self._Fs_range = Range(0, 10e3, 1, 1e3, 200)
-        self._Fs_win = RangeWidget(self._Fs_range, self.set_Fs, "frecuencia pulsos", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._Fs_win)
-        self._Fm_range = Range(0, 10e3, 10, 100, 200)
-        self._Fm_win = RangeWidget(self._Fm_range, self.set_Fm, "Frecuencia mensaje", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._Fm_win)
-        self._D_range = Range(0, 50, 1, 10, 200)
-        self._D_win = RangeWidget(self._D_range, self.set_D, "Ancho pulsos", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._D_win)
-        self._Am_range = Range(0, 10, 100e-3, 1, 200)
-        self._Am_win = RangeWidget(self._Am_range, self.set_Am, "Amplitud pulsos", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._Am_win)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_c(
             1024, #size
             samp_rate, #samp_rate
             "", #name
-            2, #number of inputs
+            1, #number of inputs
             None # parent
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
@@ -131,7 +109,7 @@ class Prueba_PAM(gr.top_block, Qt.QWidget):
             'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
         alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
             1.0, 1.0, 1.0, 1.0, 1.0]
-        styles = [3, 1, 1, 1, 1,
+        styles = [1, 1, 1, 1, 1,
             1, 1, 1, 1, 1]
         markers = [-1, -1, -1, -1, -1,
             -1, -1, -1, -1, -1]
@@ -139,7 +117,10 @@ class Prueba_PAM(gr.top_block, Qt.QWidget):
 
         for i in range(2):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                if (i % 2 == 0):
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Re{{Data {0}}}".format(i/2))
+                else:
+                    self.qtgui_time_sink_x_0.set_line_label(i, "Im{{Data {0}}}".format(i/2))
             else:
                 self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
             self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
@@ -150,24 +131,66 @@ class Prueba_PAM(gr.top_block, Qt.QWidget):
 
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
-        self.analog_sig_source_x_0 = analog.sig_source_f(samp_rate, analog.GR_COS_WAVE, Fm, Am, 0, 0)
-        self.ModulacionPAM_0 = ModulacionPAM(
-            D=D,
-            Fs=Fs,
-            samp_rate=samp_rate,
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+            1024, #size
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            0, #fc
+            samp_rate, #bw
+            "", #name
+            1,
+            None # parent
         )
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+        self.qtgui_freq_sink_x_0.set_fft_window_normalized(False)
+
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.epy_block_0 = epy_block_0.blk(additionFlag=False)
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.analog_sig_source_x_0_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 3000, 1, 0, 0)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, 1000, 1, 0, 0)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.ModulacionPAM_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.ModulacionPAM_0, 0))
-        self.connect((self.analog_sig_source_x_0, 0), (self.qtgui_time_sink_x_0, 1))
+        self.connect((self.analog_sig_source_x_0, 0), (self.epy_block_0, 0))
+        self.connect((self.analog_sig_source_x_0_0, 0), (self.epy_block_0, 1))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_freq_sink_x_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.epy_block_0, 0), (self.blocks_throttle_0, 0))
 
 
     def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "Prueba_PAM")
+        self.settings = Qt.QSettings("GNU Radio", "gnuradiop1")
         self.settings.setValue("geometry", self.saveGeometry())
         self.stop()
         self.wait()
@@ -179,42 +202,16 @@ class Prueba_PAM(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.ModulacionPAM_0.set_samp_rate(self.samp_rate)
         self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.analog_sig_source_x_0_0.set_sampling_freq(self.samp_rate)
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
-    def get_Fs(self):
-        return self.Fs
-
-    def set_Fs(self, Fs):
-        self.Fs = Fs
-        self.ModulacionPAM_0.set_Fs(self.Fs)
-
-    def get_Fm(self):
-        return self.Fm
-
-    def set_Fm(self, Fm):
-        self.Fm = Fm
-        self.analog_sig_source_x_0.set_frequency(self.Fm)
-
-    def get_D(self):
-        return self.D
-
-    def set_D(self, D):
-        self.D = D
-        self.ModulacionPAM_0.set_D(self.D)
-
-    def get_Am(self):
-        return self.Am
-
-    def set_Am(self, Am):
-        self.Am = Am
-        self.analog_sig_source_x_0.set_amplitude(self.Am)
 
 
 
-
-def main(top_block_cls=Prueba_PAM, options=None):
+def main(top_block_cls=gnuradiop1, options=None):
 
     if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
